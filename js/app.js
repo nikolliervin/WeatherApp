99917a1408a7d70e31edf7760a47028a25653956
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-07-20 22:18:44 +0200

    city fetch

--- js/app.js
+++ js/app.js
@@ -1,8 +1,30 @@
+
+
 let weather={
     'apiKey':'0179eede0cda5805bbbf0167236f129d',
-    fetchWeather:function(){
-        fetch('https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}')
+    fetchWeather:function(city){
+        fetch(
+            'https://api.openweathermap.org/data/2.5/weather?q='
+            + city 
+            +'&appid='
+            + this.apiKey
+            )
         .then((response)=>response.json())
-        .then((data)=> console.log(data))
+        .then((data) => this.displayWeather(data));
     }, 
+    displayWeather: function(data){
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        const { country } = data.sys
+        document.getElementById('cityName').innerHTML=name + ', ' + country;
+        document.getElementById('temp').innerHTML = temp + ' ' + '°C';
+        document.getElementById('description').innerHTML=convert(description);
+    }
+
+}
+
+function convert(kelvin){
+    return kelvin-273.15;
 }
